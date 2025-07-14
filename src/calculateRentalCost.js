55598592b2bf1bfb8e0aa6dfec18638e0131cb18
Vlad <rebellqwe@gmail.com>
2025-07-14 20:13:37 +0300

    add solution js

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,23 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
-}
+  const shortTerm = 3;
+  const longTerm = 7;
 
+  if (days < shortTerm) {
+    return 80;
+  }
+
+  if (days === shortTerm && days < longTerm) {
+    return 100;
+  }
+
+  if (days > shortTerm && days < longTerm) {
+    return 220;
+  }
+
+  if (days >= longTerm) {
+    return 230;
+  }
+}
 module.exports = calculateRentalCost;
